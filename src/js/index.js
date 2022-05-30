b5dbe40fddd6e766bd80b820ae7c6685c7cb799c
piwe2004 <piwe2004@gmail.com>
2022-05-30 14:24:26 +0900

    개발환경 설정 - 할일생성, 삭제 기능

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,93 @@
 import '@fortawesome/fontawesome-free/js/all.min.js';
 import '../scss/style.scss';
-console.log('todo list');
+
+class TodoList{
+
+    constructor() {
+        this.assignElement();
+        this.addEvent();
+    }
+
+    assignElement(){
+        this.inputContainerEl = document.getElementById('input-container');
+        this.inputAreaEl = this.inputContainerEl.querySelector('#input-area');
+        this.todoInputEl = this.inputAreaEl.querySelector('#todo-input');
+        this.addBtnEl = this.inputAreaEl.querySelector('#add-btn');
+        this.todoContainerEl = document.getElementById('todo-container');
+        this.todoListEl = this.todoContainerEl.querySelector('#todo-list');
+    }
+
+    addEvent(){
+        this.addBtnEl.addEventListener('click', this.onClickAddBtn.bind(this));
+        this.todoListEl.addEventListener('click', this.onClickTodoList.bind(this));
+    }
+
+    onClickTodoList(e){
+        const {target} = e;
+        const btn = target.closest('button');
+        if(btn.matches('#delete-btn')){
+            this.deleteTOdo(target);
+        }
+    }
+
+    deleteTOdo(target){
+        const todoDiv = target.closest('.todo');
+        todoDiv.addEventListener('transitionend', () => {
+            todoDiv.remove();
+        });
+        todoDiv.classList.add('delete');
+    }
+
+    onClickAddBtn(){
+        if(this.todoInputEl.value.length === 0){
+            alert('내용을 입력해 주세요.');
+            return;
+        }
+        this.createTodoElement(this.todoInputEl.value);
+    }
+
+    createTodoElement(value){
+        const todoDiv = document.createElement('div');
+        todoDiv.classList.add('todo');
+
+        const todoContent = document.createElement('input');
+        todoContent.value = value;
+        todoContent.readOnly = true;
+        todoContent.classList.add('todo-item');
+
+        const fragment = new DocumentFragment();
+        fragment.appendChild(todoContent);
+        fragment.appendChild(
+            this.createButton('complete-btn', 'complete-btn', ['fas', 'fa-check'])
+        );
+        fragment.appendChild(
+            this.createButton('edit-btn', 'edit-btn', ['fas', 'fa-edit'])
+        );
+        fragment.appendChild(
+            this.createButton('delete-btn', 'delete-btn', ['fas', 'fa-trash'])
+        );
+        fragment.appendChild(
+            this.createButton('save-btn', 'save-btn', ['fas', 'fa-save'])
+        );
+        todoDiv.appendChild(fragment);
+        this.todoListEl.appendChild(todoDiv);
+        this.todoInputEl.value="";
+        this.todoInputEl.focus();
+
+    }
+
+    createButton(btnId, btnClassName, iconClassName){
+        const btn = document.createElement('button');
+        const icon = document.createElement('i');
+        icon.classList.add(...iconClassName);
+        btn.appendChild(icon);
+        btn.id = btnId;
+        btn.classList.add(btnClassName);
+        return btn;
+    }
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const todoList = new TodoList();
+});
